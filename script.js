d08166a0918544bfb2a2582f215cae0a4748b497
Shaohao Lin <slin232@uwo.ca>
2014-09-19 12:15:30 -0400

    Added JQueryVersion

--- script.js
+++ script.js
@@ -1,35 +1,17 @@
-function load() { 
-    var index;
+$(document).ready(function(){
     var list = ["apple", "orange", "banana"];
-    for (index=0; index<list.length; index++){
-        var entry = document.createElement('li');
-        entry.appendChild(document.createTextNode(list[index]));
-        fruits.appendChild(entry);
+    for (index=0; index<list.length; index++){  
+        $("#fruits").append($("<li></li>").text(list[index]));
     }
-}
+ 
 
-function changeText2() {
-    var findmatch = false;
-    var index;        
-    var fruitList = document.getElementById('fruits');
-    var basketList = document.getElementById('basket');
-    var pickFruit = document.getElementById('newfruit').value;
-    document.getElementById('showfruit').innerHTML = pickFruit;
-    
-        for (index=0; index<fruitList.childNodes.length; index++){
-            
-            if (fruitList.childNodes[index].innerHTML == pickFruit){
-                 var entry = document.createElement('li');
-                 entry.appendChild(document.createTextNode(pickFruit));
-                 basketList.appendChild(entry);
-                 fruitList.removeChild(fruitList.childNodes[index]);
-                 findmatch = true;
-                break;
-            }           
-        }
-        if (!findmatch) {
-             document.getElementById('showfruit').innerHTML = pickFruit + " is not in the Shelf.";   
-            }
-}
-
-    
+$("input").click(function(){
+    var pickFruit = $("#newfruit").val();
+    $("#fruits").find('li').each(function(index) {
+        if(pickFruit == $(this).text()) {
+            $("#basket").append($("<li></li>").text($(this).text()));
+            $(this).remove();        
+        }  
+    });
+});
+});
